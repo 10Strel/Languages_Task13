34eab7c479b7648a2c9a0cda71552d1aa0a42099
10Strel <manowartenarrows@gmail.com>
2022-09-04 12:27:41 +0300

    Final commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
 ﻿Console.WriteLine("Программа выводит третью цифру заданного числа или сообщает, что третьей цифры нет.\r\n");
+
+bool res = int.TryParse(Console.ReadLine(), out int number);
+if (!res)
+{
+    Console.WriteLine("Ошибка ввода.\r\nВыполнение программы будет остановлено.");
+    return;
+}
+
+string tmp = Math.Abs(number).ToString();
+
+if (tmp.Length < 3)
+    Console.WriteLine($"{number}: третьей цифры нет.");
+else
+    Console.WriteLine($"Результат: {tmp[2]}");
